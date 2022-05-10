baf066ce7697bb37af196664e6e2fc2fa00a3dbc
murugan-30495 <aravind30495@gmail.com>
2022-05-10 09:31:04 +0530

    Done

---
+++ src/org/test/github.java
@@ -0,0 +1,52 @@
+package org.test;
+
+ 
+public class github {
+	
+	
+	
+	private void empId() {
+	       System.out.println("EmployeeId - 10");
+		}
+		private void empName() {
+	        
+			System.out.println("EmployeeName - Kevin");
+		}
+		private void empDob() {
+	 
+		System.out.println("EmployeeDob - 13/02/2004");
+		}   
+
+		private void empPhone() {
+	 
+		System.out.println("EmployeePhone - 97654867473");
+
+		}
+	   private void empMail() {
+		   
+	    System.out.println("EmployeeMail - xxyyy@mail.com");
+	   }
+
+	   private void empAddress() {
+	 
+		   System.out.println("EmployeeAddress - chennai");
+	 }
+	   
+	   /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 
+		github g = new github();
+		   
+		   g.empId();
+		   g.empName();
+		   g.empMail();
+		   g.empDob();
+		   g.empAddress();
+		  // e.empPhone();
+	}
+	   
+ 
+
+}
